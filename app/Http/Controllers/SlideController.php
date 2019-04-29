044b85f35ce31202629d93fea29093c7a2d56bd8
Vũ Đức Thắng <anhthangckb@gmail.com>
2019-04-29 22:45:21 +0700

    delete data dummy

--- app/Http/Controllers/SlideController.php
+++ app/Http/Controllers/SlideController.php
@@ -85,9 +85,7 @@ class SlideController extends Controller
             imagejpeg($this->resize_image('upload/slide/'.$image, 1200, 500), 'upload/slide/'.$image);
             unlink('upload/slide/'.$slide->image);
             $slide->image = $image;
-        } else {
-        	// return redirect('admin/slide/edit/'.$id)->with('loi', 'Bạn chưa chọn ảnh cần thay đổi');
-        }
+        } else {}
         $slide->save();
 	    return redirect('admin/slide/list')->with('thongbao', 'Bạn đã sửa slide thành công');
     }
